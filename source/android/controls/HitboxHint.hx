d38b489e1e68572084388937b5aa5a48e74ca9ea
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-03-29 09:38:52 -0700

    new cam

--- source/android/controls/HitboxHint.hx
+++ source/android/controls/HitboxHint.hx
@@ -1,26 +1,32 @@
 package android.controls;
 
-import flixel.group.FlxSpriteGroup;
-import flixel.FlxSprite;
+import flixel.FlxG;
 import flixel.FlxCamera;
+import flixel.FlxSprite;
+import flixel.group.FlxSpriteGroup;
 
 #if mobile
-class HitboxHint extends FlxSpriteGroup 
+class HitboxHint extends FlxSpriteGroup
 {
     public var hint:FlxSprite;
+    public var camHint:FlxCamera;
 
-    public function new(camOther:FlxCamera) 
+    public function new()
     {
         super();
 
+        camHint = new FlxCamera(0, 0, FlxG.width, FlxG.height);
+        camHint.bgColor = 0x00000000;
+        camHint.scroll.set(0, 0);
+        camHint.follow(null);
+        FlxG.cameras.add(camHint, false);
+
         var hintGraphic = Paths.image(
-            (ClientPrefs.data.hitboxStyle == 'Gradient') 
-            ? "mobile/hitbox/hitbox_hint_gradient" 
+            (ClientPrefs.data.hitboxStyle == 'Gradient')
+            ? "mobile/hitbox/hitbox_hint_gradient"
             : "mobile/hitbox/hitbox_hint",
             "shared"
         );
-
-        hint = new FlxSprite();
 
         if (hintGraphic != null)
             hint = new FlxSprite().loadGraphic(hintGraphic);
@@ -33,7 +39,7 @@ class HitboxHint extends FlxSpriteGroup
         hint.scrollFactor.set(0, 0);
         hint.alpha = ClientPrefs.data.hitboxOpacity;
         hint.visible = true;
-        hint.cameras = [camOther];
+        hint.cameras = [camHint];
 
         add(hint);
     }
